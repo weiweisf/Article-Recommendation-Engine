1e7ceaab13ba97dc2fbd02f1a72ca85e81097b23
weiweisf <wwei7@dons.usfca.edu>
2019-04-11 21:23:36 -0700

    upload the files

---
+++ doc2vec.py
@@ -0,0 +1,191 @@
+import sys
+import re
+import string
+import os
+import numpy as np
+import codecs
+
+# import sys
+# sys.setdefaultencoding('utf8')
+
+# From scikit learn that got words from:
+# http://ir.dcs.gla.ac.uk/resources/linguistic_utils/stop_words
+ENGLISH_STOP_WORDS = frozenset([
+    "a", "about", "above", "across", "after", "afterwards", "again", "against",
+    "all", "almost", "alone", "along", "already", "also", "although", "always",
+    "am", "among", "amongst", "amoungst", "amount", "an", "and", "another",
+    "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
+    "around", "as", "at", "back", "be", "became", "because", "become",
+    "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+    "below", "beside", "besides", "between", "beyond", "bill", "both",
+    "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con",
+    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done",
+    "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
+    "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+    "everything", "everywhere", "except", "few", "fifteen", "fifty", "fill",
+    "find", "fire", "first", "five", "for", "former", "formerly", "forty",
+    "found", "four", "from", "front", "full", "further", "get", "give", "go",
+    "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
+    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his",
+    "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed",
+    "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
+    "latterly", "least", "less", "ltd", "made", "many", "may", "me",
+    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
+    "move", "much", "must", "my", "myself", "name", "namely", "neither",
+    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
+    "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on",
+    "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+    "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps",
+    "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+    "seeming", "seems", "serious", "several", "she", "should", "show", "side",
+    "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
+    "something", "sometime", "sometimes", "somewhere", "still", "such",
+    "system", "take", "ten", "than", "that", "the", "their", "them",
+    "themselves", "then", "thence", "there", "thereafter", "thereby",
+    "therefore", "therein", "thereupon", "these", "they", "thick", "thin",
+    "third", "this", "those", "though", "three", "through", "throughout",
+    "thru", "thus", "to", "together", "too", "top", "toward", "towards",
+    "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
+    "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
+    "whence", "whenever", "where", "whereafter", "whereas", "whereby",
+    "wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
+    "who", "whoever", "whole", "whom", "whose", "why", "will", "with",
+    "within", "without", "would", "yet", "you", "your", "yours", "yourself",
+    "yourselves"])
+
+
+def load_glove(filename):
+    """
+    Read all lines from the indicated file and return a dictionary
+    mapping word:vector where vectors are of numpy `array` type.
+    GloVe file lines are of the form:
+
+    the 0.418 0.24968 -0.41242 0.1217 ...
+
+    So split each line on spaces into a list; the first element is the word
+    and the remaining elements represent factor components. The length of the vector
+    should not matter; read vectors of any length.
+    """
+    fdict = {}
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            linelist = line.strip('\n').split(' ')
+            fdict[linelist[0]] = np.array(linelist[1:],dtype=float)
+    return fdict
+
+
+def filelist(root):
+    """Return a fully-qualified list of filenames under root directory"""
+    allfiles = []
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            allfiles.append(os.path.join(path, name))
+    return allfiles
+
+
+def get_text(filename):
+    """
+    Load and return the text of a text file, assuming latin-1 encoding as that
+    is what the BBC corpus uses.  Use codecs.open() function not open().
+    """
+    f = codecs.open(filename, encoding='latin-1', mode='r')
+    s = f.read()
+    f.close()
+    return s
+
+
+def words(text):
+    """
+    Given a string, return a list of words normalized as follows.
+    Split the string to make words first by using regex compile() function
+    and string.punctuation + '0-9\\r\\t\\n]' to replace all those
+    char with a space character.
+    Split on space to get word list.
+    Ignore words < 3 char long.
+    Lowercase all words
+    Remove English stop words
+    """
+    regex = re.compile('[' + re.escape(string.punctuation) + '0-9\\r\\t\\n]')
+    nopunct = regex.sub(" ", text)  # delete stuff but leave at least a space to avoid clumping together
+    words = nopunct.split(" ")
+    words = [w for w in words if len(w) > 2]  # ignore a, an, to, at, be, ...
+    words = [w.lower() for w in words]
+    words = [t for t in words if t not in ENGLISH_STOP_WORDS]
+    return words
+
+
+def load_articles(articles_dirname, gloves):
+    """
+    Load all .txt files under articles_dirname and return a table (list of lists/tuples)
+    where each record is a list of:
+
+      [filename, title, article-text-minus-title, wordvec-centroid-for-article-text]
+
+    We use gloves parameter to compute the word vectors and centroid.
+
+    The filename is stripped of the prefix of the articles_dirname pulled in as
+    script parameter sys.argv[2]. E.g., filename will be "business/223.txt"
+    """
+    allfiles = filelist(articles_dirname)
+    allfiles = [f for f in allfiles if f.endswith('.txt')]
+    artslist = []
+    for fpath in allfiles:
+        s = get_text(fpath).split('\n', 1)
+        title = s[0]
+        text = s[1]
+        docvec = doc2vec(text,gloves)
+        atuple = (fpath.replace(articles_dirname+'/',''),title,
+                  text,docvec)
+        artslist.append(atuple)
+    return artslist
+
+
+def doc2vec(text, gloves):
+    """
+    Return the word vector centroid for the text. Sum the word vectors
+    for each word and then divide by the number of words. Ignore words
+    not in gloves.
+    """
+    wordlist = words(text)
+    sumvec = []
+    for w in wordlist:
+        if w in gloves:
+            sumvec.append(gloves[w])
+    return np.mean(sumvec,axis=0)
+
+
+def distances(article, articles):
+    """
+    Compute the euclidean distance from article to every other article and return
+    a list of (distance, a) tuples for all a in articles. The article is one
+    of the elements (tuple) from the articles list.
+    """
+    dists = []
+    for atc in articles:
+        if article != atc:
+            dis = np.sqrt(np.sum((article[3] - atc[3]) ** 2))
+            dists.append((dis,atc))
+    return dists
+
+def recommended(article, articles, n):
+    """
+    Return a list of the n articles (records with filename, title, etc...)
+    closest to article's word vector centroid. The article is one of the elements
+    (tuple) from the articles list.
+    """
+    dists = distances(article,articles)
+    return [a[1] for a in sorted(dists, key=lambda x: x[0])[1:6]]
+
+
+if __name__ == '__main__':
+    glove_filename = './glove/glove.6B.300d.txt'
+    articles_dirname = '/Users/weiwei/usf_classes/692dataacquisition/msds692/data/bbc'
+    # glove_filename = sys.argv[1]
+    # articles_dirname = sys.argv[2]
+
+    gloves = load_glove(glove_filename)
+    articles = load_articles(articles_dirname, gloves)
+
+    print(gloves['dog'])
+    # print(distances(articles[0],articles))
+    print(recommended(articles[0],articles,2))
---
+++ server.py
@@ -0,0 +1,60 @@
+# Launch with
+#
+# gunicorn -D --threads 4 -b 0.0.0.0:5000 --access-logfile server.log --timeout 60 server:app glove.6B.300d.txt bbc
+
+from flask import Flask, render_template
+from doc2vec import *
+import sys
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def articles():
+    """Show a list of article titles"""
+    # return render_template('articles.html')
+    return render_template('articles.html', articles=articles,
+                           topics=topics,filenames=filenames)
+
+
+
+
+@app.route("/article/<topic>/<filename>")
+def article(topic,filename):
+    """
+    Show an article with relative path filename. Assumes the BBC structure of
+    topic/filename.txt so our URLs follow that.
+    """
+    for a in articles:
+        if topic+'/'+filename == a[0]:
+            text = a[2]
+            title = a[1]
+            break
+    rec_articles = recommended(a,articles,n_best)
+    rec_topics = [a[0].split('/')[0] for a in rec_articles]
+    rec_filenames = [a[0].split('/')[1] for a in rec_articles]
+    return render_template('article.html',topic2=topic,fname2=filename,text2=text,title2=title,
+                           rec_articles2=rec_articles,rec_topics2=rec_topics,rec_filenames2=rec_filenames)
+
+
+
+
+# initialization
+glove_filename = './glove/glove.6B.300d.txt'
+articles_dirname = './bbc'
+
+# i = sys.argv.index('server:app')
+# glove_filename = sys.argv[i+1]
+# articles_dirname = sys.argv[i+2]
+
+n_best = 5
+gloves = load_glove(glove_filename)
+articles = load_articles(articles_dirname, gloves)
+# print(articles[0])
+topics = [a[0].split('/')[0] for a in articles]
+filenames = [a[0].split('/')[1] for a in articles]
+# print(filenames[0])
+
+# app.run('0.0.0.0')
+# app.run()
